bd47295d90371a11b24f223c6b94585449a857d9
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-31 14:51:09 -0700

    finish lesson

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 var request = require('request');
 var async = require('async');
 
-// functions
-function fn1(callback) {
-  var initial = 55;
-  callback(null, initial);
-}
-function fn2(num1, callback) {
-  num1 += 5;
-  callback(null, num1);
-}
-function fn3(num2, callback) {
-  num2 += 40;
-  callback(null, num2);
-}
+// // functions
+// function fn1(callback) {
+  // var initial = 55;
+  // callback(null, initial);
+// }
+// function fn2(num1, callback) {
+  // num1 += 5;
+  // callback(null, num1);
+// }
+// function fn3(num2, callback) {
+  // num2 += 40;
+  // callback(null, num2);
+// }
 
-async.waterfall([fn1, fn2, fn3], function(err, results) {
-  console.log('Done!');
+// async.waterfall([fn1, fn2, fn3], function(err, results) {
+  // console.log('Done!');
+  // console.log(results);
+// });
+// example of what DOESN'T work
+var urls = ['http://www.reddit.com/search.json?q=politics',
+'http://www.reddit.com/search.json?q=puppies',
+'http://www.reddit.com/search.json?q=beyonce'];
+
+// create an iterator to get first title of urls
+var getFirstTitle = function(url, callback) {
+  request(url, function(err, res, data) {
+    var firstTitle = JSON.parse(data).data.children[0].data.title;
+    callback(null, firstTitle);
+  });
+};
+
+async.concat(urls, getFirstTitle, function(err, results) {
   console.log(results);
 });
-// // example of what DOESN'T work
-// var urls = ['http://www.google.com', 'http://www.yelp.com',
-// 'http://www.twitter.com'];
-// var allData = [];
-
-// urls.forEach(function(url) {
-  // request(url, function(err, res, data) {
-    // allData.push(data);
-  // });
-// });
-
-// console.log(allData);
